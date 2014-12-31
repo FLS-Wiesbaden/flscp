0daa6553d1fedc18aa2b404e271201b498e5997e
Lukas Schreiner <lukas.schreiner@fls-wiesbaden.de>
2014-12-31 18:13:20 +0100

    QMessageBox is out of QtGui!!

--- flscp/flscp.py
+++ flscp/flscp.py
@@ -47,8 +47,8 @@ workDir = os.path.dirname(os.path.realpath(__file__))
 
 ##### CONFIGURE #####
 # connection
-RPCHOST 		= 'cp.lschreiner.de' 
-#RPCHOST 		= 'cp.fls-wiesbaden.de' 
+#RPCHOST 		= 'cp.lschreiner.de' 
+RPCHOST 		= 'cp.fls-wiesbaden.de' 
 RPCPORT 		= 10027
 RPCPATH			= 'RPC2'
 # ssl connection
@@ -1369,12 +1369,12 @@ class FLScpMainWindow(QMainWindow):
 				self.close()
 				return
 			except Exception as e:
-				QtGui.QMessageBox.critical(
+				QMessageBox.critical(
 					self, _translate('MainWindow', 'Unbekannter Fehler beim Verbinden', None), 
 					_translate('MainWindow', 
 						'Der Server konnte den Verbindungsversuch nicht erfolgreich abarbeiten',
 						None),
-					QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok
+					QMessageBox.Ok, QMessageBox.Ok
 				)
 				self.close()
 				return
